9e2b26306710f4ceb4be9833fb3dacfb25fbb996
nijiaqi <583522219@qq.com>
2019-06-14 21:02:41 +0800

    1212121212

---
+++ mydubbo-remoting/mydubbo-remoting-api/src/main/java/com/njq/mydubbo/remoting/api/transport/AbstractEndpoint.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.njq.mydubbo.remoting.api.transport;
+
+import com.njq.mydubbo.common.Parameters;
+import com.njq.mydubbo.common.Resetable;
+import com.njq.mydubbo.common.URL;
+import com.njq.mydubbo.common.extension.ExtensionLoader;
+import com.njq.mydubbo.common.logger.Logger;
+import com.njq.mydubbo.common.logger.LoggerFactory;
+import com.njq.mydubbo.remoting.api.ChannelHandler;
+import com.njq.mydubbo.remoting.api.Codec;
+import com.njq.mydubbo.remoting.api.Codec2;
+import com.njq.mydubbo.remoting.api.Constants;
+import com.njq.mydubbo.remoting.api.transport.codec.CodecAdapter;
+
+import static com.njq.mydubbo.common.constants.CommonConstants.DEFAULT_TIMEOUT;
+import static com.njq.mydubbo.common.constants.CommonConstants.TIMEOUT_KEY;
+
+/**
+ * AbstractEndpoint
+ */
+public abstract class AbstractEndpoint extends AbstractPeer implements Resetable {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractEndpoint.class);
+
+    private Codec2 codec;
+
+    private int timeout;
+
+    private int connectTimeout;
+
+    public AbstractEndpoint(URL url, ChannelHandler handler) {
+        super(url, handler);
+        this.codec = getChannelCodec(url);
+        this.timeout = url.getPositiveParameter(TIMEOUT_KEY, DEFAULT_TIMEOUT);
+        this.connectTimeout = url.getPositiveParameter(Constants.CONNECT_TIMEOUT_KEY, Constants.DEFAULT_CONNECT_TIMEOUT);
+    }
+
+    protected static Codec2 getChannelCodec(URL url) {
+        String codecName = url.getParameter(Constants.CODEC_KEY, "telnet");
+        if (ExtensionLoader.getExtensionLoader(Codec2.class).hasExtension(codecName)) {
+            return ExtensionLoader.getExtensionLoader(Codec2.class).getExtension(codecName);
+        } else {
+            return new CodecAdapter(ExtensionLoader.getExtensionLoader(Codec.class)
+                    .getExtension(codecName));
+        }
+    }
+
+    @Override
+    public void reset(URL url) {
+        if (isClosed()) {
+            throw new IllegalStateException("Failed to reset parameters "
+                    + url + ", cause: Channel closed. channel: " + getLocalAddress());
+        }
+        try {
+            if (url.hasParameter(TIMEOUT_KEY)) {
+                int t = url.getParameter(TIMEOUT_KEY, 0);
+                if (t > 0) {
+                    this.timeout = t;
+                }
+            }
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+        }
+        try {
+            if (url.hasParameter(Constants.CONNECT_TIMEOUT_KEY)) {
+                int t = url.getParameter(Constants.CONNECT_TIMEOUT_KEY, 0);
+                if (t > 0) {
+                    this.connectTimeout = t;
+                }
+            }
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+        }
+        try {
+            if (url.hasParameter(Constants.CODEC_KEY)) {
+                this.codec = getChannelCodec(url);
+            }
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+        }
+    }
+
+    @Deprecated
+    public void reset(Parameters parameters) {
+        reset(getUrl().addParameters(parameters.getParameters()));
+    }
+
+    protected Codec2 getCodec() {
+        return codec;
+    }
+
+    protected int getTimeout() {
+        return timeout;
+    }
+
+    protected int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+}
